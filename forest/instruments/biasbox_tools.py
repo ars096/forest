8ba83b7f4a43b09ee6591c4c5bd00729211a1a11
ars096 <ars096@gmail.com>
2014-12-21 05:40:49 +0900

    closed #1, created biasbox and its system check

--- forest/instruments/biasbox_tools.py
+++ forest/instruments/biasbox_tools.py
@@ -1,4 +1,6 @@
 
+import itertools
+import numpy
 import pyinterface
 
 class biasbox(object):
@@ -16,7 +18,6 @@ class biasbox(object):
         
         Args
         ====
-        
         Required
         --------
         < bias : float : (mV) >
@@ -25,8 +26,8 @@ class biasbox(object):
             
             optional parameters
             - - - - - - - - - -      
-             (A) None : If no parameters are specified, <bias> will apply to
-                        all mixers.
+             (A) None : If no parameters are specified, <bias> will be applied
+                        to all mixers.
                   
              (B) box, ch : Use box number (1-4) and ch (1-4) to specify the
                            target mixer.
@@ -74,81 +75,345 @@ class biasbox(object):
         ========
         >>> b.bias_set(0)
         >>> b.bias_set(1.2)
+        >>> b.bias_set(0.5, box=1)
         >>> b.bias_set(0.5, box=1, ch=2)
+        >>> b.bias_set(0.234, beam=1)
+        >>> b.bias_set(0.234, beam=2, pol='V')
         >>> b.bias_set(0.234, beam=3, pol='H', dsbunit=2)
         """
-        bias = bias_value(bias)
-        bias = sis_voltage(sis=bias)
+        bias = sis_bias_value(bias)
+        bias = bias_voltage_changer(sis=bias)
         argc = len(kwargs)
         if argc==0:
             self.daq.analog_output(bias.box)
         else:
-            if argc==2:
-                box = biasbox_num(kwargs['box'])
-                ch = biasbox_ch_num(kwargs['ch'])
+            if kwargs.get('box') is not None:
+                box = biasbox_num(kwargs.get('box'))
+                ch = biasbox_ch_num(kwargs.get('ch'))
                 sisch = biasbox_ch_mapper(box=box, ch=ch)
-            elif argc==3:
-                beam = beam_num(kwargs['beam'])
-                pol = pol_char(kwargs['pol'])
-                dsbunit = dsbunit_num(kwargs['dsbunit'])
+            else:
+                beam = beam_num(kwargs.get('beam'))
+                pol = pol_char(kwargs.get('pol'))
+                dsbunit = dsbunit_num(kwargs.get('dsbunit'))
                 sisch = biasbox_ch_mapper(beam=beam, pol=pol, dsbunit=dsbunit)
                 pass
             self.daq.analog_output(bias.box, sisch.vout)
             pass
         return
         
+    def bias_get(self, **kwargs):
+        """
+        Get bias voltage and current of SIS mixer(s).
+        
+        Args
+        ====
+        Required
+        --------
+        Nothing.
+        
+        If no parameters are specified, bias voltages and currents
+        for all SIS mixers will be returned.
+        Target mixer(s) are specified in the optional parameters.
+        
+            optional parameters
+            - - - - - - - - - -      
+             (A) None : If no parameters are specified, the target mixers are
+                        specified as all SIS mixers.
+             
+             (B) box, ch : Use box number (1-4) and ch (1-4) to specify the
+                           target mixer.
+         
+             (C) beam, pol, dsbunit : Use beam number (1-4),
+                                      polarization (H, V),
+                                      and DSB unit number (1, 2) to specify
+                                      the target mixer.
+        
+        optional
+        --------
+        (Option A)
+            Get all mixer's bias parameters.
+            No additional parameters are required.
+        
+        (Option B)
+            Get bias parameters of <box>:<ch> mixer.
+            < box : int : 1-4 >
+                Box number to specify the target mixer.
+                Box number should be 1-4.
+        
+            < ch : int : 1-4 >
+                Ch number to specify the target mixer.
+                Ch number should be 1-4.
+        
+        (Option C)
+            Get bias parameters of <beam>:<pol>:<num> mixer.
+            < beam : int : 1-4 >
+                Beam number to specify the target mixer.
+                Beam number should be 1-4.
+        
+            < pol : str : 'H', 'V' >
+                Polarization to specify the target mixer.
+                Polarization should be 'H' or 'V'.
+        
+            < dsbunit : int : 1,2 >
+                DSB unit number to specify the target mixer.
+                DSB unit number should be 1 or 2.
+        
+        Returns
+        =======
+        (Option A, B, C)
+            < bias_parameters : list(float) : (mV, mA) >
+                A list of bias parameters list of all SIS mixers.
+                bias_parameters[0] : list of bias voltages (mV)
+                bias_parameters[1] : list of bias currents (mA)
+        
+        (Option B, C)
+            Note: If you specified a SIS mixer (i.e., specified all arguments;
+            for example, in a case of option B, <box> and <ch>), a list of 
+            float values will be returned. (See Examples below.)
+                
+        Examples
+        ========
+        >>> b.bias_get()
+        [[0.12, 0.12, ..., 0.34, 0.32],
+         [0.12, 0.12, ..., 0.34, 0.32]]
+        
+        >>> b.bias_get(box=1)
+        [[0.12, 0.12, 0.34, 0.32],
+         [0.12, 0.12, 0.34, 0.32]]
+        
+        >>> b.bias_get(box=2, ch=3)
+        [0.22, 0.11]
+        
+        >>> b.bias_get(beam=1)
+        [[0.12, 0.12, 0.34, 0.32],
+         [0.12, 0.12, 0.34, 0.32]]
+        
+        >>> b.bias_get(beam=2, pol='H')
+        [[0.12, 0.12],
+         [0.12, 0.12]]
+        
+        >>> b.bias_get(beam=2, pol='V', ch=3)
+        [0.22, 0.11]
+        """
+        argc = len(kwargs)
+        if argc==0:
+            sisch = biasbox_ch_mapper()
+        else:
+            if kwargs.get('box') is not None:
+                box = biasbox_num(kwargs.get('box'))
+                ch = biasbox_ch_num(kwargs.get('ch'))
+                sisch = biasbox_ch_mapper(box=box, ch=ch)
+            else:
+                beam = beam_num(kwargs.get('beam'))
+                pol = pol_char(kwargs.get('pol'))
+                dsbunit = dsbunit_num(kwargs.get('dsbunit'))
+                sisch = biasbox_ch_mapper(beam=beam, pol=pol, dsbunit=dsbunit)
+                pass
+            pass
+        ret = self.daq.analog_input()
+        vin= numpy.array(ret)[sisch.vin]
+        vin = bias_voltage_changer(box=vin)
+        iin= numpy.array(ret)[sisch.iin]
+        iin = bias_current_changer(box=iin)
+        return vin.sis, iin.sis
+        
+    def bias_sweep(self, sweep_values, **kwargs):
+        bias = sis_bias_array(sweep_values)
+        bias = bias_voltage_changer(sis=bias)
+        argc = len(kwargs)
+        if argc==0:
+            sisch = biasbox_ch_mapper()
+        else:
+            if kwargs.get('box') is not None:
+                box = biasbox_num(kwargs.get('box'))
+                ch = biasbox_ch_num(kwargs.get('ch'))
+                sisch = biasbox_ch_mapper(box=box, ch=ch)
+            else:
+                beam = beam_num(kwargs.get('beam'))
+                pol = pol_char(kwargs.get('pol'))
+                dsbunit = dsbunit_num(kwargs.get('dsbunit'))
+                sisch = biasbox_ch_mapper(beam=beam, pol=pol, dsbunit=dsbunit)
+                pass
+            pass
+        ret = self.daq.analog_sweep(bias.box, sisch.vout)
+        vin = numpy.array(ret)[:,sisch.vin]
+        vin = bias_voltage_changer(box=vin)
+        iin = numpy.array(ret)[:,sisch.iin]
+        iin = bias_current_changer(box=iin)
+        return vin.sis, iin.sis
+
+
+
 
 # ==============
 # Helper Classes
 # ==============
 
-# Value Container Class
-# =====================
+# Value Checker Class
+# ===================
 
-# Value Container Base
-# --------------------
-class str_container(object):
+# Value Checker Base
+# ------------------
+class value_checker_base(object):
+    val = None
+    
+    def __add__(self, x):
+        return self.val.__add__(x)
+        
+    def __sub__(self, x):
+        return self.val.__sub__(x)
+        
+    def __mul__(self, x):
+        return self.val.__mul__(x)
+
+    def __truediv__(self, x):
+        return self.val.__truediv__(x)
+
+    def __floordiv__(self, x):
+        return self.val.__floordiv__(x)
+
+    def __mod__(self, x):
+        return self.val.__mod__(x)
+
+    def __divmod__(self, x):
+        return self.val.__divmod__(x)
+
+    def __pow__(self, x):
+        return self.val.__pow__(x)
+
+    def __radd__(self, x):
+        return self.val.__radd__(x)
+        
+    def __rsub__(self, x):
+        return self.val.__rsub__(x)
+        
+    def __rmul__(self, x):
+        return self.val.__rmul__(x)
+
+    def __rtruediv__(self, x):
+        return self.val.__rtruediv__(x)
+
+    def __rfloordiv__(self, x):
+        return self.val.__rfloordiv__(x)
+
+    def __rmod__(self, x):
+        return self.val.__rmod__(x)
+
+    def __rdivmod__(self, x):
+        return self.val.__rdivmod__(x)
+
+    def __rpow__(self, x):
+        return self.val.__rpow__(x)
+
+    def __neg__(self, x):
+        return self.val.__neg__(x)
+
+    def __pos__(self, x):
+        return self.val.__pos__(x)
+
+    def __abs__(self, x):
+        return self.val.__abs__(x)
+
+    def __eq__(self, x):
+        return self.val == x
+
+    def __ne__(self, x):
+        return self.val != x
+
+    def __lt__(self, x):
+        return self.val < x
+
+    def __le__(self, x):
+        return self.val <= x
+
+    def __gt__(self, x):
+        return self.val > x
+
+    def __ge__(self, x):
+        return self.val >= x
+
+    def __bool__(self, x):
+        return self.val.__bool__(x)
+        
+    def __index__(self):
+        return self.val.__index__()
+        
+    def __key__(self):
+        return self.val.__key__()
+
+    def __str__(self):
+        return self.val.__str__()
+
+    def __int__(self):
+        return self.val.__int__()
+
+    def __float__(self):
+        return self.val.__float__()
+
+
+class str_container(value_checker_base):
     val = 'A'
     required = [0]
     error_msg = "value should be 'A'."
-
+    
     def __init__(self, val):
-        if not val in self.required:
-            raise ValueError(self.error_msg + ' (val=%s)'%(val))
-        self.val = val
+        if val is None:
+            self.val = val
+        else:
+            if not val in self.required:
+                raise ValueError(self.error_msg + ' (val=%s)'%(val))
+            self.val = str(val)
+            pass
         pass
         
-    def __str__(self):
-        return self.val
-
-class int_container(object):
+class int_container(value_checker_base):
     val = 0
     required = [0]
     error_msg = 'value should be 0.'
 
     def __init__(self, val):
-        if not val in self.required:
-            raise ValueError(self.error_msg + ' (val=%d)'%(val))
-        self.val = val
+        if val is None:
+            self.val = val
+        else:
+            if not val in self.required:
+                raise ValueError(self.error_msg + ' (val=%d)'%(val))
+            self.val = int(val)
+            pass
         pass
-        
-    def __int__(self):
-        return self.val
 
-class float_container(object):
+class float_container(value_checker_base):
     val = 0.0
     required_min = -1.0
     required_max = 1.0
     error_msg = 'value should be -1.0 - 1.0.'
     
     def __init__(self, val):
-        if not(self.required_min < val < self.required_max):
-            raise ValueError(self.error_msg + ' (val=%f)'%(val))
-        self.val = val
+        if val is None:
+            self.val = val
+        else:
+            if not(self.required_min <= val <= self.required_max):
+                raise ValueError(self.error_msg + ' (val=%f)'%(val))
+            self.val = float(val)
+            pass
         pass
-        
-    def __float__(self):
-        return self.val
+    
+class array_container(value_checker_base):
+    val = []
+    required_min = -1.0
+    required_max = 1.0
+    error_msg = 'value should be -1.0 - 1.0.'
+    
+    def __init__(self, val):
+        if val is None:
+            self.val = val
+        else:
+            _min = self.required_min
+            _max = self.required_max
+            if not(((_min <= val) & (val <= _max)).all()):
+                raise ValueError(self.error_msg + ' (val=%f)'%(val))
+            self.val = numpy.array(val)
+            pass
+        pass
 
 # Values 
 # ------
@@ -157,11 +422,10 @@ class sis_bias_value(float_container):
     requred_max = 1.0
     error_msg = 'SIS bias should be in 0.0 - 1.0 mV'
 
-class sis_voltage(bias_changer):
-    sis_to_box_conversion_factor = 1.
-    
-class sis_current(bias_changer):
-    sis_to_box_conversion_factor = 1.
+class sis_bias_array(array_container):
+    required_min = 0.0
+    requred_max = 1.0
+    error_msg = 'SIS bias should be in 0.0 - 1.0 mV'
 
 class biasbox_num(int_container):
     required = [1, 2, 3, 4]
@@ -192,12 +456,18 @@ class bias_changer(object):
     def __init__(self, sis=None, box=None):
         if sis is not None:
             self.sis = sis
-            self.box = sis * self.sis_to_boxconversion_factor
+            self.box = sis * self.sis_to_box_conversion_factor
         else:
             self.box = box
-            self.sis = box / self.sis_to_boxconversion_factor
+            self.sis = box / self.sis_to_box_conversion_factor
             pass
         pass
+
+class bias_voltage_changer(bias_changer):
+    sis_to_box_conversion_factor = 1.
+
+class bias_current_changer(bias_changer):
+    sis_to_box_conversion_factor = 1.
 
 
 # Bias CH Changer
@@ -208,46 +478,85 @@ class biasbox_ch_mapper(object):
                  3:{1:9, 2:10, 3:11, 4:12},
                  4:{1:13, 2:14, 3:15, 4:16}}
     
-    beam_map = {1:{'H':{1:1, 2:2}, 'V'{1:3, 2:4}},
-                2:{'H':{1:5, 2:6}, 'V'{1:7, 2:8}},
-                3:{'H':{1:9, 2:10}, 'V'{1:11, 2:12}},
-                4:{'H':{1:13, 2:14}, 'V'{1:15, 2:16}}}
+    beam_map = {1:{'H':{1:1, 2:2}, 'V':{1:3, 2:4}},
+                2:{'H':{1:5, 2:6}, 'V':{1:7, 2:8}},
+                3:{'H':{1:9, 2:10}, 'V':{1:11, 2:12}},
+                4:{'H':{1:13, 2:14}, 'V':{1:15, 2:16}}}
     
     vout_map = [None, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     vin_map = [None, 1, 3, 5, 7, 17, 19, 21, 23, 33, 35, 37, 39, 49, 51, 53, 55]
     iin_map = [None, 0, 2, 4, 6, 16, 18, 20, 22, 32, 34, 36, 38, 48, 50, 52, 54]
     
     def __init__(self, box=None, ch=None, beam=None, pol=None, dsbunit=None):
-        if box is not None:
-            self.sis = self.boxch_map[box][ch]
+        if box != None:
+            box = int(box)
+            if ch != None:
+                ch = int(ch)
+                self.sis = [self.boxch_map[box][ch]]
+            else:
+                self.sis = self.boxch_map[box].values()
+                pass
+        elif beam != None:
+            beam = int(beam)
+            if pol != None:
+                pol = str(pol)
+                if dsbunit != None:
+                    dsbunit = int(dsbunit)
+                    self.sis = [self.beam_map[beam][pol][dsbunit]]
+                else:
+                    self.sis = self.beam_map[beam][pol].values()
+                    pass
+            else:
+                sis_list = [s.values() for s in self.beam_map[beam].values()]
+                self.sis = list(itertools.chain.from_iterable(sis_list))
+                pass
+            pass
         else:
-            self.sis = self.beam_map[beam][pol][dsbunit]
+            self.sis = range(1, 17)
             pass
         self._set_parameters()
         pass
         
     def _set_parameters(self):
-        for box_id, chs in boxch_map.items():
+        self.box = []
+        self.ch = []
+        for box_id, chs in self.boxch_map.items():
             for ch_id, val in chs.items():
-                if val==self.sis: 
-                    self.box = box_id
-                    self.ch = ch_id
+                if val in self.sis: 
+                    self.box.append(box_id)
+                    self.ch.append(ch_id)
                     pass
                 continue
             continue
-        for beam_id, pols in beam_map.items():
+            
+        self.beam = []
+        self.pol = []
+        self.dsbunit = []
+        for beam_id, pols in self.beam_map.items():
             for pol_id, units in pols.items():
                 for dsbunit_id, val in units.items():
-                    if val==self.sis: 
-                        self.beam = beam_id
-                        self.pol = pol_id
-                        self.dsbunit = dsbunit_id
+                    if val in self.sis: 
+                        self.beam.append(beam_id)
+                        self.pol.append(pol_id)
+                        self.dsbunit.append(dsbunit_id)
                         pass
                     continue
                 continue
             continue
-        self.vout = vout_map[self.sis]
-        self.vin = vin_map[self.sis]
-        self.iin = iin_map[self.sis]
+        self.vout = [self.vout_map[s] for s in self.sis]
+        self.vin = [self.vin_map[s] for s in self.sis]
+        self.iin = [self.iin_map[s] for s in self.sis]
+        
+        if len(self.sis)==1:
+            self.sis = self.sis[0]
+            self.box = self.box[0]
+            self.ch = self.ch[0]
+            self.beam = self.beam[0]
+            self.pol = self.pol[0]
+            self.dsbunit = self.dsbunit[0]
+            self.vout = self.vout[0]
+            self.vin = self.vin[0]
+            self.iin = self.iin[0]
+            pass
         return
 
---
+++ forest/systemcheck_biasbox.py
@@ -0,0 +1,34 @@
+
+import time
+import numpy
+import pylab
+import instruments
+
+biasbox = instruments.biasbox()
+
+data = numpy.linspace(0, 1, 101)
+v, i = biasbox.bias_sweep(data)
+biasbox.bias_set(0)
+
+# --
+
+ch = instruments.biasbox_tools.biasbox_ch_mapper()
+
+pylab.rcParams['font.size'] = 8
+
+fig = pylab.figure(figsize=(16,12))
+ax = [fig.add_subplot(4, 4, j) for j in range(1, 17)]
+for _ax, _n, _box, _ch, _beam, _pol, _unit \
+    in zip(ax, ch.sis, ch.box, ch.ch, ch.beam, ch.pol, ch.dsbunit):
+    _ax.plot(v[:,_n-1], i[:,_n-1], '+')
+    _ax.text(0.1, 0.8, 'Box: %d, CH: %d'%(_box, _ch), transform=_ax.transAxes)
+    _ax.text(0.1, 0.7, 'Beam: %d, Pol: %s, Unit: %d'%(_beam, _pol, _unit),
+             transform=_ax.transAxes)
+    _ax.grid(True)
+    _ax.set_xlabel('Bias Voltage (mV)')
+    _ax.set_ylabel('Bias Current (mA)')
+    continue
+
+fig.suptitle('systemcheck_biasbox.py :: %s'%(time.strftime('%Y/%m/%d %H:%M:%S')))
+fig.savefig('systemcheck_biasbox.%s.png'%(time.strftime('%Y%m%d.%H%M%S')))
+
