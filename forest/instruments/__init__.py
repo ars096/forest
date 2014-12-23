17046c629b8577a071e1c1d6f9df57e64779e1b6
ars096 <ars096@gmail.com>
2014-12-23 17:05:13 +0900

    closed #7, created speana

--- forest/instruments/__init__.py
+++ forest/instruments/__init__.py
@@ -1,4 +1,11 @@
 
+from speana_tools import speana1
+from speana_tools import speana2
+from speana_tools import speana3
+from speana_tools import speana
+
 from biasbox_tools import biasbox
+
 from loatt_tools import lo_att
+
 from switch_tools import switch
