924ab2171df64fd7aa0cfbc208250ebde5cb732a
ars096 <ars096@gmail.com>
2014-12-27 00:15:15 +0900

    add password

--- database_scripts/create_forest_users.sql
+++ database_scripts/create_forest_users.sql
@@ -5,10 +5,14 @@
 # Create users for forest project.
 #
 
-CREATE USER forest_reader;
-CREATE USER forest_writer;
+GRANT SELECT
+      ON forest.* 
+      TO forest_reader
+      IDENTIFIED "forest";
 
-GRANT SELECT ON forest.* TO forest_reader;
-GRANT SELECT,UPDATE,INSERT ON forest.* TO forest_writer;
+GRANT SELECT,UPDATE,INSERT 
+      ON forest.* 
+      TO forest_writer
+      IDENTIFIED "forestroot";
 
 
