8a5c8f7060b1614eaa1c6ace46a113e6fc2f84a7
ars096 <ars096@gmail.com>
2014-12-26 22:30:51 +0900

    fixed #22

--- bin/update_scripts.py
+++ bin/update_scripts.py
@@ -8,12 +8,12 @@ import shutil
 
 # Create directories
 # ------------------
-if not os.path.exists('~/git'): os.mkdir('~/git')
-if not os.path.exists('~/python'): os.mkdir('~/python')
-if not os.path.exists('~/bin'): os.mkdir('~/bin')
-if not os.path.exists('~/database'): os.mkdir('~/database')
-if not os.path.exists('~/database/scripts'): os.mkdir('~/database/scripts')
-if not os.path.exists('~/data'): os.mkdir('~/data')
+if not os.path.exists('/home/forest/git'): os.mkdir('/home/forest/git')
+if not os.path.exists('/home/forest/python'): os.mkdir('/home/forest/python')
+if not os.path.exists('/home/forest/bin'): os.mkdir('/home/forest/bin')
+if not os.path.exists('/home/forest/database'): os.mkdir('/home/forest/database')
+if not os.path.exists('/home/forest/database/scripts'): os.mkdir('/home/forest/database/scripts')
+if not os.path.exists('/home/forest/data'): os.mkdir('/home/forest/data')
 
 
 # Download Latest Python Modules
@@ -21,28 +21,28 @@ if not os.path.exists('~/data'): os.mkdir('~/data')
 
 # forest
 # ------
-if not os.path.exists('~/git/forest'):
-    os.system('git clone https://github.com/ars096/forest.git ~/git/forest/')
+if not os.path.exists('/home/forest/git/forest'):
+    os.system('git clone https://github.com/ars096/forest.git /home/forest/git/forest/')
 else:
-    os.chdir('~/git/forest')
+    os.chdir('/home/forest/git/forest')
     os.system('git pull')
     pass
 
 # pymeasure2
 # ----------
-if not os.path.exists('~/git/pymeasure2'):
-    os.system('git clone https://github.com/ars096/pymeasure2.git ~/git/pymeasure2/')
+if not os.path.exists('/home/forest/git/pymeasure2'):
+    os.system('git clone https://github.com/ars096/pymeasure2.git /home/forest/git/pymeasure2/')
 else:
-    os.chdir('~/git/pymeasure2')
+    os.chdir('/home/forest/git/pymeasure2')
     os.system('git pull')
     pass
 
 # pyinterface
 # -----------
-if not os.path.exists('~/git/pyinterface'):
-    os.system('git clone https://github.com/ars096/pyinterface.git ~/git/pyinterface/')
+if not os.path.exists('/home/forest/git/pyinterface'):
+    os.system('git clone https://github.com/ars096/pyinterface.git /home/forest/git/pyinterface/')
 else:
-    os.chdir('~/git/pyinterface')
+    os.chdir('/home/forest/git/pyinterface')
     os.system('git pull')
     pass
 
@@ -52,18 +52,18 @@ else:
 
 # forest
 # ------
-os.system('cp -r ~/git/forest/forest ~/python/')
-os.system('cp ~/git/forest/bin/* ~/bin/')
-os.system('cp ~/git/forest/database_scripts/* ~/database/scripts/')
-os.system('chmod 755 ~/bin/*')
+os.system('cp -r /home/forest/git/forest/forest /home/forest/python/')
+os.system('cp /home/forest/git/forest/bin/* /home/forest/bin/')
+os.system('cp /home/forest/git/forest/database_scripts/* /home/forest/database/scripts/')
+os.system('chmod 755 /home/forest/bin/*')
 
 # pymeasure2
 # ----------
-os.system('cp -r ~/git/pymeasure2/pymeasure ~/python/')
+os.system('cp -r /home/forest/git/pymeasure2/pymeasure /home/forest/python/')
 
 # pyinterface
 # -----------
-os.system('cp -r ~/git/pyinterface/pyinterface ~/python/')
+os.system('cp -r /home/forest/git/pyinterface/pyinterface /home/forest/python/')
 
 
 
