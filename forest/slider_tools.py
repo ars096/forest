1934050db3cfbd92f38b3a6fdf009e5207b001b1
ars096 <ars096@gmail.com>
2015-01-07 02:14:15 +0900

    temp

--- forest/slider_tools.py
+++ forest/slider_tools.py
@@ -1,7 +1,7 @@
 
 import pyinterface
 
-class slider(object):
+class slider_controller(object):
     pos_sky = 0
     pos_sig = 19000
     pos_r = 25000
@@ -11,11 +11,28 @@ class slider(object):
     acc = 1000
     dec = 1000
     
+    error = []
+    
+    position = ''
+    
+    cosmos_flag = False
+    cosmos_recv = ''
+    
     def __init__(self, move_org=True):
         self.mtr = pyinterface.create_gpg7204(1)
         if move_org: self.move_org()
+        self.start_cosmos_server()
         pass
         
+    def print_msg(self, msg):
+        print(msg)
+        return
+        
+    def print_error(self, msg):
+        self.error.append(msg)
+        self.print_msg('!!!! ERROR !!!! ' + msg)
+        return
+    
     def move_org(self):
         """
         Move to ORG position.
@@ -36,6 +53,7 @@ class slider(object):
         """
         self.mtr.do_output(3)
         self.mtr.set_org()
+        self.position = 'ORG'
         return
 
     def _move(self, dist, lock):
@@ -69,6 +87,7 @@ class slider(object):
         >>> s.move_r()
         """
         self._move(self.pos_r, lock)
+        self.position = 'R'
         return
     
     def move_sky(self, lock=True):
@@ -92,6 +111,7 @@ class slider(object):
         >>> s.move_sky()
         """
         self._move(self.pos_sky, lock)
+        self.position = 'SKY'
         return
     
     def move_sig(self, lock=True):
@@ -115,6 +135,7 @@ class slider(object):
         >>> s.move_sig()
         """
         self._move(self.pos_sig, lock)
+        self.position = 'SIG'
         return
     
     def unlock_brake(self):
@@ -166,3 +187,128 @@ class slider(object):
         """
         self.mtr.do_output(1)
         return
+
+    def start_cosmos_server(self):
+        cs = threading.Thread(target=self._start_cosmos_server)
+        cs.start()
+        return
+        
+    def _start_cosmos_server(self):
+        self.print_msg('INFO: start cosmos server')
+        
+        while True:
+            try:
+                self._cosmos_server()
+            except Exception as e:
+                self.print_msg('**********************************************')
+                self.print_error('cosmos server except error: %s'%(e))
+                self.print_msg('**********************************************')
+                self.print_msg('INFO: restart cosmos_server')
+                continue
+            break
+        self.print_msg('INFO: stop cosmos server')
+        return
+
+    def _cosmos_server(self):
+        pass
+    
+    """
+    def _cosmos_server(self):
+        server = socket.socket()
+        server.settimeout(1)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.print_msg('INFO: cosmos: (bind) %s:%s'%('', cosmos_server_port))
+        server.bind(('', cosmos_server_port))
+        server.listen(1)
+        
+        while True:
+            self.cosmos_flag = False
+            
+            try:
+                client, client_address = server.accept()
+                self.print_msg('INFO: cosmos: Accept from %s'%(str(client_address)))
+                client.settimeout(1)
+            
+            except socket.timeout:
+                if self.shutdown_flag:
+                    self.print_msg('INFO: cosmos: detect shutdown signal')
+                    self.print_msg('INFO: cosmos: break')
+                    break
+                continue
+            
+            self.cosmos_flag = True
+
+            while True:
+                if self.shutdown_flag: 
+                    self.print_msg('INFO: cosmos: detect shutdown signal')
+                    self.print_msg('INFO: cosmos: break')
+                    break
+                
+                try:
+                    ret = client.recv(24)
+                except socket.timeout:
+                    continue
+                except socket.error, e:
+                    self.print_error('cosmos: %s'%str((e.errno, e.message, e.strerror)))
+                    self.print_msg('INFO: cosmos: connection break')
+                    break
+                
+                #print('RECV: %s'%(repr(ret)))
+                self.cosmos_recv = ret
+                if ret == '': 
+                    self.print_msg('INFO: cosmos: connection break')
+                    break
+                
+                ret = ret.strip('\0').split('\t')
+                operate = int(ret[0])
+                return_flag = int(ret[1])
+                timestamp = ret[2]
+                target = float(ret[3])
+                self.cosmos_angle = target
+                
+                if self.tracking_count > 4: is_tracking = 1
+                else: is_tracking = 0
+                
+                msg = '[%s] op=%s ret=%s t=%s prog=%s '%(time.strftime('%Y/%m/%d %H:%M:%S'),
+                                                         ret[0], ret[1], ret[2], ret[3])
+                msg += 'real=%+06.1f diff=%.1f vel=%+06.1f track=%d'%(self.real_angle,
+                                                                      self.residual,
+                                                                      self.real_vel,
+                                                                      self.tracking_count)
+                self.print_msg(msg)
+                
+                if operate == 1: 
+                    self.move(target)
+                    pass
+                
+                if return_flag == 1:
+                    err_no = 0
+                    err_msg = ''
+                    msg = '%d\t%s\t%+06.1f\t%02d\t%50s\0'%(is_tracking, self.real_timestamp,
+                                                           self.real_angle, err_no, err_msg)
+                    #print('SEND: %s'%(repr(msg)))
+                    self.cosmos_send = msg
+                    client.send(msg)
+                    pass
+                continue
+            continue
+        return
+    """
+
+def slider():
+    client = pyinterface.server_client_wrapper.control_client_wrapper(
+        slider_controller, '192.168.40.13', 4004)
+    return client
+
+def slider_monitor():
+    client = pyinterface.server_client_wrapper.monitor_client_wrapper(
+        slider_controller, '192.168.40.13', 4104)
+    return client
+
+def start_slider_server():
+    slider = slider_controller()
+    server = pyinterface.server_client_wrapper.server_wrapper(slider,
+                                                              '', 4004, 4104)
+    server.start()
+    return server
+
