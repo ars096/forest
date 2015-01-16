a8e5cdd6833113ce0a5231a304d4e6a406c807e6
ars096 <ars096@gmail.com>
2015-01-16 23:51:10 +0900

    create forest/script/init_fin.py

--- forest/script/__init__.py
+++ forest/script/__init__.py
@@ -1,2 +1,3 @@
 
 
+from base import forest_script_base
--- forest/script/base.py
+++ forest/script/base.py
@@ -1,9 +1,11 @@
 
 import sys
+import time
 import forest
 
 class forest_script_base(object):
     method = ''
+    ver = ''
     
     def __init__(self):
         self.log = forest.db_writer('operation_log')
@@ -25,10 +27,19 @@ class forest_script_base(object):
         
         return
 
-    def operation_start(self, args=''):
+    def operation_start(self, args='', logfile=''):
         self.log.insert(keydict={'flag': 'START',
                                  'method': self.method,
                                  'args': args})
+        
+        if logfile != '':
+            self.stdout.set_output_file(logfile)
+            self.stderr.set_output_file(logfile)
+        else:
+            self.stdout.reset()
+            self.stderr.reset()
+            pass
+        
         return
         
     def operation_done(self):
@@ -36,6 +47,54 @@ class forest_script_base(object):
                                  'method': self.method})
         return
         
+    def open_sis_biasbox(self):
+        self.stdout.write('Opening SIS bias box ... ')
+        sis = forest.biasbox()
+        self.stdout.write('ok')
+        self.stdout.nextline()
+        return sis
+
+    def open_lo_sg(self):
+        self.stdout.write('Opening 1st LO SG ... ')
+        lo_sg = None
+        self.stdout.write('NG')
+        self.stdout.nextline()
+        return lo_sg
+    
+    def open_lo_att(self):
+        self.stdout.write('Opening 1st LO Attenuator ... ')
+        lo_att = forest.loatt()
+        self.stdout.write('ok')
+        self.stdout.nextline()
+        return lo_att
+    
+    def open_irr_sg(self):
+        self.stdout.write('Opening IRR Signal SG ... ')
+        irr_sg = None
+        self.stdout.write('NG')
+        self.stdout.nextline()
+        return irr_sg
+    
+    def open_rxrot(self):
+        self.stdout.write('Opening Rx Rotator ... ')
+        rxrot = forest.rx_rotator()
+        self.stdout.write('ok')
+        self.stdout.nextline()
+        return rxrot
+    
+    def open_slider(self):
+        self.stdout.write('Opening Sliding Chopper ... ')
+        slider = forest.slider()
+        self.stdout.write('ok')
+        self.stdout.nextline()
+        return slider
+    
+        
+        
+    def run(self, *args, **kwargs):
+        pass
+        
+
 
 class logger(object):
     table = ''
@@ -49,7 +108,7 @@ class logger(object):
         pass
     
     def reset(self):
-        self.history = ''
+        self.history = []
         self.tmp = ''
         return
         
@@ -67,17 +126,25 @@ class logger(object):
     def write(self, msg):
         self.tmp += msg
         sys.stdout.write(msg)
+        self.flush()
         return
         
     def nextline(self):
-        self.db.insert(keydict={self.colname: self.tmp})
+        msg = self.tmp
+        msg2 = time.strftime('[%Y/%m/%d.%H:%M:%S] ') + msg
+        
+        self.db.insert(keydict={self.colname: msg})
+        
         if self.output_file != '':
-            open(self.output_file, 'a').write(self.tmp+'\n')
+            open(self.output_file, 'a').write(msg2+'\n')
             pass
-        self.history.append(self.tmp)
-        self.tmp = ''
-        sys.stdout.write('\n')
+        
+        self.history.append(msg2)
+        
+        sys.stdout.write('\r%s\n'%(msg2))
         self.flush()
+        
+        self.tmp = ''
         return
         
     def flush(self):
