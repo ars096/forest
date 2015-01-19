0a5c0f4811203b10cc6aedc5c29d00eff90e33b8
ars096 <ars096@gmail.com>
2015-01-19 20:58:02 +0900

    add load_sis_config function

--- forest/helper.py
+++ forest/helper.py
@@ -1,6 +1,7 @@
 
 import os
 import time
+import ConfigParser
 
 def mkdirs(path):
     if os.path.exists(path): return False
@@ -18,6 +19,53 @@ def filepath_generator(name):
         if fname.find('%s')!=-1: fname = fname%(ts)
         return os.path.join(savedir, fname)
     return filepath
+
+
+def load_sis_config(lo_freq, config_file_name='FOREST2014.cnf'):
+    config_file_dir = '/home/forest/tuning_parameters'
+    mixer_data_dir = 'mixer_unit_data'
+    confpath = os.path.join(config_file_dir, config_file_name)
+    
+    conf = ConfigParser.SafeConfigParser()
+    conf.read(confpath)
+    
+    params = {}
+    for unit in conf.options('combination'):
+        unitname = conf.get('combination', unit)
+        unitconfpath = os.path.join(config_file_dir, mixer_data_dir, 
+                                    unitname+'.cnf')
+        unitconf = ConfigParser.SafeConfigParser()
+        unitconf.read(unitconfpath)
+        
+        section = '%d GHz'%(lo_freq)
+        if not unitconf.has_section(section):
+            print('WARN: %s has no parameters for %d GHz.'%(unitname, lo_freq))
+            print('WARN: Use default parameters.')
+            lo_freq_str = unitconf.get('info', 'default')
+            try:
+                lo_freq = int(lo_freq_str.strip(' GHZghz_-'))
+            except ValueError, e:
+                raise('ERROR: %s.info.default is wrong. (%s)'%(unitname, 
+                                                               lo_freq_str))
+            print('WARN: Default is LO_Freq. = %d GHz.'%(lo_freq))
+            section = '%d GHz'%(lo_freq)
+            if not unitconf.has_section(section):
+                raise('ERROR: %s has no parameters for %d GHz.'%(unitname, lo_freq))
+            pass
+            
+        bias1 = float(unitconf.get(section, 'bias1'))
+        bias2 = float(unitconf.get(section, 'bias2'))
+        lo_att = float(unitconf.get(section, 'lo_att'))
+        j_type = unitconf.get('info', 'L_sis_id').split('-')[2]
+        
+        print('%s: bias1 = %f,  bias2 = %f,  lo_att = %f'%(unit, bias1, bias2,
+                                                           lo_att))
+        params[unit] = {'bias1': bias1, 'bias2': bias2, 'lo_att': lo_att, 'J-type': j_type}
+        continue
+        
+    return params
+
+
 
 
 # print methods
