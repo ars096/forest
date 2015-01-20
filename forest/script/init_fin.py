79256d6a6cd48b908897ebb4a2e0d4ed886b56aa
ars096 <ars096@gmail.com>
2015-01-20 14:17:33 +0900

    add irr_sg, switch, and speana functions

--- forest/script/init_fin.py
+++ forest/script/init_fin.py
@@ -37,6 +37,8 @@ class initialize(base.forest_script_base):
         lo_sg = self.open_lo_sg()
         lo_att = self.open_lo_att()
         irr_sg = self.open_irr_sg()
+        switch = self.open_switch()
+        speana = self.open_speana()
         rxrot = self.open_rxrot()
         slider = self.open_slider()
         
@@ -56,12 +58,10 @@ class initialize(base.forest_script_base):
         self.stdout.write('1st LO SG : Set -130 dBm ... ')
         lo_sg.power_set(-130)
         self.stdout.write('ok')
-        self.stdout.nextline()
         
         self.stdout.write('1st LO SG : Set 10 GHz ... ')
         lo_sg.freq_set(10, 'GHz')
         self.stdout.write('ok')
-        self.stdout.nextline()
         
         self.stdout.write('1st LO SG : Set output off ... ')
         lo_sg.output_off()
@@ -73,13 +73,32 @@ class initialize(base.forest_script_base):
         self.stdout.write('ok')
         self.stdout.nextline()
         
-        # TODO: uncomment IRR SG
-        """
         self.stdout.write('IRR SG : Set output off ... ')
         irr_sg.output_off()
         self.stdout.write('ok')
         self.stdout.nextline()
-        """
+        
+        self.stdout.write('IF Switch : Set ch 1 ... ')
+        switch.ch_set_all(1)
+        self.stdout.write('ok')
+        self.stdout.nextline()
+        
+        self.stdout.write('Speana : Preset ... ')
+        speana.scpi_reset()
+        self.stdout.write('ok')
+        
+        self.stdout.write('Speana : Set start freq 4 GHz.')
+        speana.frequency_start_set(4, 'GHz')
+        self.stdout.write('ok')
+        
+        self.stdout.write('Speana : Set stop freq 12 GHz.')
+        speana.frequency_stop_set(12, 'GHz')
+        self.stdout.write('ok')
+        
+        self.stdout.write('Speana : Set resolution BW 3 MHz.')
+        speana.resolution_bw_set(3, 'MHz')
+        self.stdout.write('ok')
+        self.stdout.nextline()
         
         self.stdout.write('Rx Rotator : Move to 0 deg ... ')
         rxrot.move(0)
@@ -159,6 +178,8 @@ class finalize(base.forest_script_base):
         lo_sg = self.open_lo_sg()
         lo_att = self.open_lo_att()
         irr_sg = self.open_irr_sg()
+        switch = self.open_switch()
+        speana = self.open_speana()
         rxrot = self.open_rxrot()
         slider = self.open_slider()
         
@@ -195,14 +216,21 @@ class finalize(base.forest_script_base):
         self.stdout.write('ok')
         self.stdout.nextline()
         
-        # TODO: uncomment IRR SG
-        """
         self.stdout.write('IRR SG : Set output off ... ')
         irr_sg.output_off()
         self.stdout.write('ok')
         self.stdout.nextline()
-        """
         
+        self.stdout.write('IF Switch : Set ch 1 ... ')
+        switch.ch_set_all(1)
+        self.stdout.write('ok')
+        self.stdout.nextline()
+        
+        self.stdout.write('Speana : Preset ... ')
+        speana.scpi_reset()
+        self.stdout.write('ok')
+        self.stdout.nextline()
+                
         self.stdout.write('Rx Rotator : Move to 0 deg ... ')
         rxrot.move(0)
         # TODO: lock until movement is done.
