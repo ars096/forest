02035566516ecffcce1352b857baf7d96f2bda7c
ars096 <ars096@gmail.com>
2015-03-05 22:22:40 +0900

    add if_freq parameter

--- bin/exp_rsky_with_LO_att_sweep.py
+++ bin/exp_rsky_with_LO_att_sweep.py
@@ -18,7 +18,7 @@ start = 5
 stop = 25
 step = 0.2
 Thot = 293.0 # K
-
+if_freq = 8
 
 # Argument Parser
 # ===============
@@ -34,6 +34,8 @@ p.add_argument('--step', type=float,
                help='Step of sweep Att level in mA. default is %.2f mA.'%(step))
 p.add_argument('--Thot', type=float,
                help='Hot temperature in K. default is %.2f K.'%(Thot))
+p.add_argument('--if_freq', type=float,
+               help='IF freq. to be evaruated. default is %.2f GHz.'%(if_freq))
 
 args = p.parse_args()
 
@@ -41,6 +43,7 @@ if args.start is not None: start = args.start
 if args.stop is not None: stop = args.stop
 if args.step is not None: step = args.step
 if args.Thot is not None: Thot = args.Thot
+if args.if_freq is not None: if_freq = args.if_freq
 
 
 # Run Script
@@ -49,6 +52,6 @@ if args.Thot is not None: Thot = args.Thot
 import forest.script
 
 script = forest.script.rsky_with_lo_att_sweep()
-script.run(start, stop, step, Thot)
+script.run(start, stop, step, Thot, if_freq)
 
 
